5633cea0cf9dfa263134f271cd2db5d719ea7bd3
gabriel teixeira <gabriel2001td@gmail.com>
2023-10-09 16:49:10 -0300

    ...

---
+++ src/Operadores/Operadores.java
@@ -0,0 +1,77 @@
+package Operadores;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+       boolean con1 = true;
+       boolean con2 = false;
+       if(con1 && con2){
+        System.out.println("as duas condição são verdadeiras");
+       }
+       if(con1 || con2){
+        System.out.println("uma das codições e verdadeira");
+       }
+       System.out.println("fim");
+
+       
+        /*  String no1 = "gabriel";
+        String no2 = "gabriel";
+        
+        System.out.println(no1 == no2);
+
+       int n1 = 1;
+       int n2 = 2;
+        boolean simNao = n1 == n2;
+
+        if(n1 < n2){
+            System.out.println("a nossa condição e vadadeira");
+        }
+        
+        System.out.println("numero e igual? " + simNao);
+        simNao = n1 != n2;
+        System.out.println("o numero e diferente? " + simNao);
+        simNao = n1 > n2;
+        System.out.println("o numero e maior " + simNao);*/
+
+       
+       
+        /* int a, b;
+       a = 5;
+       b = 6;
+       String resltado = a==b ?"vadaderio" : "falso";
+       System.out.println(resltado);*/
+
+       
+        /*int a, b;
+        a = 5;
+        b = 6;
+        String resultado = "";
+        if(a==b)
+            resultado = "Verdadeiro";
+        else
+            resultado = "falso";
+        
+        System.out.println(resultado);*/
+
+       
+        /* boolean variavel = true;
+        System.out.println(!variavel);
+        System.out.println(variavel);*/
+
+
+        /*int numero = 5;
+        numero++;
+        System.out.println(numero);
+
+        numero--;
+        System.out.println(numero);*/
+
+
+        /* int numero = 5;
+        numero = - numero;
+        System.out.println( numero);
+
+        numero = numero * -1;
+        System.out.println(numero); */
+    }
+}
---
+++ src/Variaves/TiposVariaves.java
@@ -0,0 +1,14 @@
+package Variaves;
+public class TiposVariaves {
+    public static void main(String[] args) {
+       int numero = 2;
+       numero = 8;
+       System.out.print(numero);
+
+        System.out.print("\n");
+      
+        final double valor_de_pi = 3.14;
+        System.out.print(valor_de_pi);
+
+    }
+}
